c6110366a736b9d09389c1ee196312abdb1efbd2
Jemimah Adamu <jemimah.adamu@ontariotechu.net>
2025-11-20 11:16:49 -0500

    Refactored Task model to use MongoDB with Mongoose

--- models/Task.js
+++ models/Task.js
@@ -1,52 +1,11 @@
 // models/Task.js
-let tasks = [];
-let nextId = 1;
+const mongoose = require("mongoose");
 
-class Task {
-    constructor(title, description, dueDate, status) {
-        this._id = nextId++; // unique ID
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.status = status || "Not Started"; // default status
-    }
-}
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    dueDate: String,
+    status: { type: String, default: "Not Started" }
+});
 
-// Get all tasks
-function getAllTasks() {
-    return tasks;
-}
-
-// Get a single task by _id
-function getTaskById(id) {
-    return tasks.find(t => t._id == id);
-}
-
-// Add a new task
-function addTask(taskData) {
-    const newTask = new Task(
-        taskData.title,
-        taskData.description,
-        taskData.dueDate,
-        taskData.status
-    );
-    tasks.push(newTask);
-}
-
-// Update an existing task
-function updateTask(id, updatedData) {
-    const task = getTaskById(id);
-    if (task) {
-        task.title = updatedData.title;
-        task.description = updatedData.description;
-        task.dueDate = updatedData.dueDate;
-        task.status = updatedData.status;
-    }
-}
-
-// Delete a task
-function deleteTask(id) {
-    tasks = tasks.filter(t => t._id != id);
-}
-
-module.exports = { getAllTasks, getTaskById, addTask, updateTask, deleteTask };
+module.exports = mongoose.model("Task", taskSchema);
